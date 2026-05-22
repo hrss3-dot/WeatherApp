7c04d1ed9cf90f11814c0fd872441f64490aa45e
hugorafael.00 <hugo36rafael@gmail.com>
2026-05-22 17:02:34 -0300

    Prática 2 - Parte 1

---
+++ app/src/main/java/com/example/weatherapp/ui/HomePage.kt
@@ -0,0 +1,33 @@
+package com.example.weatherapp.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun HomePage(modifier: Modifier = Modifier.Companion) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.Companion.Blue)
+            .wrapContentSize(Alignment.Companion.Center)
+    ) {
+        Text(
+            text = "Home",
+            fontWeight = FontWeight.Companion.Bold,
+            color = Color.Companion.White,
+            modifier = modifier.align(Alignment.Companion.CenterHorizontally),
+            textAlign = TextAlign.Companion.Center,
+            fontSize = 20.sp
+        )
+    }
+}
