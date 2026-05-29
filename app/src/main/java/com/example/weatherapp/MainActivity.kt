99cbae62895ff75e504f3a4ec3df90cc5cce57a3
hugorafael.00 <hugo36rafael@gmail.com>
2026-05-29 16:21:32 -0300

    Prática 3 - Parte 3

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -21,8 +21,13 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import com.example.weatherapp.model.MainViewModel
 import androidx.navigation.compose.rememberNavController
+import com.example.weatherapp.ui.CityDialog
 import com.example.weatherapp.ui.nav.BottomNavBar
 import com.example.weatherapp.ui.nav.BottomNavItem
 import com.example.weatherapp.ui.nav.MainNavHost
@@ -35,8 +40,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             val navController = rememberNavController()
+
             val viewModel : MainViewModel by viewModels()
+            var showDialog by remember { mutableStateOf(false) }
             WeatherAppTheme {
+                if (showDialog) CityDialog(
+                    onDismiss = { showDialog = false },
+                    onConfirm = { city ->
+                        if (city.isNotBlank()) viewModel.add(city)
+                        showDialog = false
+                    })
+
                 Scaffold(
                     topBar = {
                         TopAppBar(
@@ -61,7 +75,7 @@ class MainActivity : ComponentActivity() {
                         BottomNavBar(navController = navController, items)
                     },
                     floatingActionButton = {
-                        FloatingActionButton(onClick = { }) {
+                        FloatingActionButton(onClick = { showDialog = true }) {
                             Icon(Icons.Default.Add, contentDescription = "Adicionar")
                         }
                     }
